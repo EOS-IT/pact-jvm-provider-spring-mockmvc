e9db15fef980e073b51994ed8df92d26f7a6ad12
Peter Salchow <p.salchow.extern@eos-it-services.com>
2016-09-26 16:48:03 +0200

    Added PactLoader tests and java doc comments

--- src/main/java/de/eosit/fx/pact/provider/PactLoader.java
+++ src/main/java/de/eosit/fx/pact/provider/PactLoader.java
@@ -20,15 +20,71 @@ public class PactLoader {
 
     private static final String FILE_EXTENSION = ".json";
 
+    /**
+     * Loads the {@link Pact} from the given <code>pactResource</code>. The
+     * <code>pactResource</code>-Parameter is provided in a form like:
+     * <ul>
+     * <li>"file:../path/to/pact/a.json" or "file:/temp/pactfolder/b.json" for
+     * file system related paths</li>
+     * <li>"classpath:pactfolder/c.json" for classpath related paths</li>
+     * <li>"WEB_INF/pacts/d.json" for relative file system paths. This might not
+     * be supported by the underlying implementation.</li>
+     * </ul>
+     *
+     * @param pactResource
+     *            The resource location in the format described above.
+     * @return Returns the found {@link Pact} from the given resource.
+     */
     public static Pact loadPactByResource(String pactResource) {
         Resource resource = new DefaultResourceLoader().getResource(pactResource);
         return PactReader.loadPact(resourceToInStream(resource));
     }
 
+    /**
+     * Loads all {@link Pact}s from "*.json" files within the given
+     * <code>pactFolder</code> and all of its sub-folders recursively. The
+     * <code>pactFolder</code>-Parameter is provided in a form like:
+     * <ul>
+     * <li>"file:../path/to/pact" or "file:/temp/pactfolder" for file system
+     * related paths</li>
+     * <li>"classpath:pactfolder" for classpath related paths</li>
+     * <li>"WEB_INF/pacts" for relative file system paths. This might not be
+     * supported by the underlying implementation.</li>
+     * </ul>
+     *
+     * A call to this function is equivalent to call
+     * <code>loadPactsByFile(pactFolder, true)</code>
+     *
+     * @param pactFolder
+     *            The resource folder location in the format described above.
+     * @return Returns the found {@link Pact}s within the given folder
+     *         (including sub-folders).
+     */
     public static List<Pact> loadPactsByResourceFolder(String pactFolder) {
         return loadPactsByResourceFolder(pactFolder, true);
     }
 
+    /**
+     * Loads all {@link Pact}s from "*.json" files within the given
+     * <code>pactFolder</code>. The <code>recursive</code>-Parameter controls if
+     * sub-folders are recursively scanned or not. The
+     * <code>pactFolder</code>-Parameter is provided in a form like:
+     * <ul>
+     * <li>"file:../path/to/pact" or "file:/temp/pactfolder" for file system
+     * related paths</li>
+     * <li>"classpath:pactfolder" for classpath related paths</li>
+     * <li>"WEB_INF/pacts" for relative file system paths. This might not be
+     * supported by the underlying implementation.</li>
+     * </ul>
+     *
+     * @param pactFolder
+     *            The resource folder location in the format described above.
+     * @param recursive
+     *            Set to <code>true</code> if all sub-folders should be scanned
+     *            for pact files recursively. If set to <code>false</code> only
+     *            the "*.json" files within the specified folder are considered.
+     * @return Returns the found {@link Pact}s within the given folder.
+     */
     public static List<Pact> loadPactsByResourceFolder(String pactFolder, boolean recursive) {
         StringBuilder locationPattern = new StringBuilder(pactFolder);
         if (recursive) {
@@ -48,10 +104,42 @@ public class PactLoader {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Loads all {@link Pact}s from the given <code>file</code>. If the given
+     * <code>file</code>-Parameter points to a real file it is loaded directly.
+     * If it is a folder it will be scanned for "*.json" files. All of its
+     * sub-folders are scanned recursively as well.
+     *
+     * A call to this function is equivalent to call
+     * <code>loadPactsByFile(file, true)</code>
+     *
+     * @param file
+     *            The file pointing to a pact file or a folder containing pact
+     *            files.
+     * @return Returns the found {@link Pact}s.
+     */
     public static List<Pact> loadPactsByFile(File file) {
         return loadPactsByFile(file, true);
     }
 
+    /**
+     * Loads all {@link Pact}s from the given <code>file</code>. If the given
+     * <code>file</code>-Parameter points to a real file it is loaded directly.
+     * If it is a folder it will be scanned for "*.json" files. If the
+     * <code>recursive</code>-Parameter is set to <code>true</code>, all of the
+     * folders sub-folders are scanned recursively as well.
+     *
+     * @param file
+     *            The file pointing to a pact file or a folder containing pact
+     *            files.
+     * @param recursive
+     *            Set to <code>true</code> if all sub-folders should be scanned
+     *            for pact files recursively. If set to <code>false</code> only
+     *            the "*.json" files within the specified folder are considered.
+     *            This parameter has only an effect if the given
+     *            <code>file</code> is a folder.
+     * @return Returns the found {@link Pact}s.
+     */
     public static List<Pact> loadPactsByFile(File file, boolean recursive) {
         if (file == null || !file.exists()) {
             return newArrayList();
@@ -71,7 +159,16 @@ public class PactLoader {
         return newArrayList();
     }
 
-    public Pact loadPactGeneric(Object source) {
+    /**
+     * Directly load the {@link Pact} by calling
+     * {@link PactReader#loadPact(Object)}.
+     *
+     * @param source
+     *            The source of the pact. This can be a URI, File or direct
+     *            Pact-Json.
+     * @return The loaded {@link Pact}.
+     */
+    public static Pact loadPactGeneric(Object source) {
         return PactReader.loadPact(source);
     }
 
--- src/test/java/de/eosit/fx/pact/provider/PactLoaderTest.java
+++ src/test/java/de/eosit/fx/pact/provider/PactLoaderTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -15,23 +16,134 @@ import org.junit.rules.TemporaryFolder;
 import com.google.common.collect.Lists;
 
 import au.com.dius.pact.model.Pact;
+import groovy.json.JsonException;
 
 public class PactLoaderTest {
 
     @Rule
     public TemporaryFolder tempFolder = new TemporaryFolder();
 
-    @Test
-    public void getResources() throws Exception {
+    private File subfolder;
+
+    @Before
+    public void setup() throws Exception {
         writePactContent(tempFolder.newFile("1.json"));
         writePactContent(tempFolder.newFile("2.json"));
         writePactContent(tempFolder.newFile("3.txt"));
-        File subfolder = tempFolder.newFolder("subfolder");
+        subfolder = tempFolder.newFolder("subfolder");
         writePactContent(new File(subfolder, "4.json"));
         writePactContent(new File(subfolder, "5.txt"));
+    }
 
+    @Test
+    public void loadPactByResource() throws Exception {
+        Pact pact = PactLoader
+                .loadPactByResource("file:" + new File(tempFolder.getRoot().getPath(), "1.json").getPath());
+        Assert.assertNotNull(pact);
+
+        pact = PactLoader.loadPactByResource("file:" + new File(subfolder.getPath(), "4.json").getPath());
+        Assert.assertNotNull(pact);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void loadPactByResourceNotExisting() throws Exception {
+        PactLoader.loadPactByResource("file:" + new File(tempFolder.getRoot().getPath(), "6.json").getPath());
+    }
+
+    @Test
+    public void loadPactsByResourceFolderRecursive() throws Exception {
         List<Pact> pacts = PactLoader.loadPactsByResourceFolder("file:" + tempFolder.getRoot().getPath());
         Assert.assertEquals(3, pacts.size());
+
+        pacts = PactLoader.loadPactsByResourceFolder("file:" + subfolder.getPath());
+        Assert.assertEquals(1, pacts.size());
+    }
+
+    @Test
+    public void loadPactsByResourceFolderNonRecursive() throws Exception {
+        List<Pact> pacts = PactLoader.loadPactsByResourceFolder("file:" + tempFolder.getRoot().getPath(), false);
+        Assert.assertEquals(2, pacts.size());
+
+        pacts = PactLoader.loadPactsByResourceFolder("file:" + subfolder.getPath(), false);
+        Assert.assertEquals(1, pacts.size());
+    }
+
+    @Test
+    public void loadPactsByResourceFolderNotExisting() throws Exception {
+        List<Pact> pacts = PactLoader.loadPactsByResourceFolder("file:" + subfolder.getPath() + "_temp");
+        Assert.assertEquals(0, pacts.size());
+    }
+
+    @Test
+    public void loadPactsByFileFolderRecursive() throws Exception {
+        List<Pact> pacts = PactLoader.loadPactsByFile(tempFolder.getRoot());
+        Assert.assertEquals(3, pacts.size());
+
+        pacts = PactLoader.loadPactsByFile(subfolder);
+        Assert.assertEquals(1, pacts.size());
+    }
+
+    @Test
+    public void loadPactsByFileFolderNonRecursive() throws Exception {
+        List<Pact> pacts = PactLoader.loadPactsByFile(tempFolder.getRoot(), false);
+        Assert.assertEquals(2, pacts.size());
+
+        pacts = PactLoader.loadPactsByFile(subfolder, false);
+        Assert.assertEquals(1, pacts.size());
+    }
+
+    @Test
+    public void loadPactsByFile() throws Exception {
+        List<Pact> pacts = PactLoader.loadPactsByFile(new File(tempFolder.getRoot().getPath(), "1.json"));
+        Assert.assertEquals(1, pacts.size());
+
+        pacts = PactLoader.loadPactsByFile(new File(subfolder.getPath(), "4.json"));
+        Assert.assertEquals(1, pacts.size());
+    }
+
+    @Test
+    public void loadPactsByFileNonRecursive() throws Exception {
+        List<Pact> pacts = PactLoader.loadPactsByFile(new File(tempFolder.getRoot().getPath(), "1.json"), false);
+        Assert.assertEquals(1, pacts.size());
+
+        pacts = PactLoader.loadPactsByFile(new File(subfolder.getPath(), "4.json"), false);
+        Assert.assertEquals(1, pacts.size());
+    }
+
+    @Test
+    public void loadPactsByFileNotExisting() throws Exception {
+        List<Pact> pacts = PactLoader.loadPactsByFile(new File(tempFolder.getRoot().getPath(), "6.json"));
+        Assert.assertEquals(0, pacts.size());
+    }
+
+    @Test
+    public void loadPactsByFileNotExistingNonRecursive() throws Exception {
+        List<Pact> pacts = PactLoader.loadPactsByFile(new File(tempFolder.getRoot().getPath(), "6.json"), false);
+        Assert.assertEquals(0, pacts.size());
+    }
+
+    @Test
+    public void loadPactGeneric() throws Exception {
+        Pact pact = PactLoader.loadPactGeneric(new File(tempFolder.getRoot().getPath(), "1.json"));
+        Assert.assertNotNull(pact);
+    }
+
+    @Test(expected = JsonException.class)
+    public void loadPactGenericNotExisting() throws Exception {
+        Pact pact = PactLoader.loadPactGeneric(new File(tempFolder.getRoot().getPath(), "6.json"));
+        Assert.assertNotNull(pact);
+    }
+
+    @Test
+    public void loadPactGenericFileString() throws Exception {
+        Pact pact = PactLoader.loadPactGeneric(new File(tempFolder.getRoot().getPath(), "1.json").getPath());
+        Assert.assertNotNull(pact);
+    }
+
+    @Test
+    public void loadPactGenericFromPactJsonString() throws Exception {
+        Pact pact = PactLoader.loadPactGeneric(getRandomPact());
+        Assert.assertNotNull(pact);
     }
 
     private File writePactContent(File file) throws IOException {
