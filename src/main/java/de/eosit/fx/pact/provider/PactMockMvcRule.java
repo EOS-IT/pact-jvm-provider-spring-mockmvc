ef4ffc4953a52e149d668e180aae255ebdccbd4f
Peter Salchow <p.salchow.extern@eos-it-services.com>
2016-09-27 15:59:10 +0200

    Added more testa and updated java doc

--- src/main/java/de/eosit/fx/pact/provider/PactMockMvcRule.java
+++ src/main/java/de/eosit/fx/pact/provider/PactMockMvcRule.java
@@ -10,20 +10,60 @@ import java.util.stream.Stream;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.springframework.test.web.servlet.MockMvc;
 
 import au.com.dius.pact.model.Pact;
 
 import static com.google.common.collect.Sets.newHashSet;
 
+/**
+ * The {@link PactMockMvcRule} allows to execute and verify a request described
+ * by pact against the expected response. A test method must configure:
+ * <ol>
+ * <li>The {@link MockMvc} to use</li>
+ * <li>And a provider state by either configuring the rule or by using the
+ * annotation {@link ProviderState}. If both methods are used in parallel the
+ * state configured on the rule has precedence.</li>
+ * </ol>
+ *
+ * <p>
+ * Example of usage:
+ *
+ * <pre>
+ * &#064;RunWith(SpringRunner.class)
+ * &#064;WebMvcTest(controllers = MyController.class)
+ * public static class PactTest {
+ *     &#064;Rule
+ *     public PactMockMvcRule pactRule = PactMockMvcRule.create().withFile("file:pacts/myPact.json").build();
+ *
+ *     &#064;Autowired
+ *     private MockMvc mockMvc;
+ *
+ *     &#064;Test
+ *     &#064;ProviderState("my provider state")
+ *     public void testInteraction() throws Exception {
+ *         pactRule.configure().setMockMvc(mockMvc).setContextPath("/app");
+ *     }
+ * }
+ * </pre>
+ */
 public class PactMockMvcRule implements TestRule {
 
     private Set<Pact> pacts = newHashSet();
     private PactTestRunner runner = null;
 
+    /**
+     * Constructs a {@link PactMockMvcRule} without any available {@link Pact}s.
+     * So this rule will not execute any requests.
+     */
     public PactMockMvcRule() {
         this(null);
     }
 
+    /**
+     * Constructs a {@link PactMockMvcRule} with the given {@link Pact}s
+     * available for executing requests.
+     */
     public PactMockMvcRule(Iterable<Pact> pacts) {
         if (pacts == null) {
             this.pacts = newHashSet();
@@ -55,19 +95,45 @@ public class PactMockMvcRule implements TestRule {
         };
     }
 
+    /**
+     * Retrieves the provider from the {@link ProviderState} annotation on the
+     * test method.
+     *
+     * @param description
+     *            The test description.
+     * @return The provider state if the annotation is present or an empty
+     *         {@link Optional} if not found.
+     */
     protected Optional<String> retrieveDefaultProviderState(Description description) {
         ProviderState stateAnnotation = description.getAnnotation(ProviderState.class);
         return Optional.ofNullable(stateAnnotation).map(s -> s.value());
     }
 
+    /**
+     * Returns the internally used {@link PactTestRunner} to configure the
+     * parameters.
+     *
+     * @return The internally used {@link PactTestRunner} instance.
+     */
     public PactTestRunner configure() {
         return runner;
     }
 
+    /**
+     * Creates a {@link PactMockMvcRuleBuilder} to build a
+     * {@link PactMockMvcRule}.
+     *
+     * @return The {@link PactMockMvcRuleBuilder} instance for building the
+     *         rule.
+     */
     public static PactMockMvcRuleBuilder create() {
         return new PactMockMvcRuleBuilder();
     }
 
+    /**
+     * A builder for the {@link PactMockMvcRule} that helps to extract
+     * {@link Pact}s from files and / or folders.
+     */
     public static class PactMockMvcRuleBuilder {
         private Set<Pact> pacts = new HashSet<>();
         private Optional<String> consumer = Optional.empty();
--- src/main/java/de/eosit/fx/pact/provider/PactTestRunner.java
+++ src/main/java/de/eosit/fx/pact/provider/PactTestRunner.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+import org.junit.experimental.results.ResultMatchers;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -24,11 +25,18 @@ import au.com.dius.pact.model.Response;
 
 import static com.google.common.collect.Sets.newHashSet;
 
+/**
+ * Instances of this class are responsible to verify a certain interaction
+ * between a consumer and a producer. It is based on a set of {@link Pact}s from
+ * which the {@link Interaction} is retrieved by the provider state. For this
+ * {@link Interaction} a spring mock mvc request is build from the interactions
+ * request parameter. The request is sent to the provided {@link MockMvc} and
+ * the response is validated against the response parameters of the interaction.
+ */
 public class PactTestRunner {
 
-    protected Optional<MockMvc> mockMvc = Optional.empty();
-
     private Set<Pact> pacts = newHashSet();
+    private Optional<MockMvc> mockMvc = Optional.empty();
     private Optional<String> consumer = Optional.empty();
     private Optional<String> provider = Optional.empty();
     private Optional<String> providerState = Optional.empty();
@@ -37,21 +45,41 @@ public class PactTestRunner {
     private Set<ResultMatcher> resultMatchers = newHashSet();
     private Consumer<? super ResultActions> responseCallback = null;
 
-    public PactTestRunner() {
-    }
-
+    /**
+     * Constructor setting the available {@link Pact}s.
+     *
+     * @param pacts
+     *            The available {@link Pact}s.
+     */
     public PactTestRunner(Iterable<Pact> pacts) {
         if (pacts != null) {
             this.pacts = newHashSet(pacts);
         }
     }
 
+    /**
+     * Constructor setting the available {@link Pact}.
+     *
+     * @param pact
+     *            The available {@link Pact}.
+     */
     public PactTestRunner(Pact pact) {
         if (pact != null) {
             this.pacts = newHashSet(pact);
         }
     }
 
+    /**
+     * This method does all the work. Depending on the parameters set, an
+     * {@link Interaction} is retrieved from the available {@link Pact}s and the
+     * described request is send to the {@link MockMvc}. The response is
+     * validated against the response information from the {@link Interaction}.
+     * Any configured callbacks will be called to intercept the execution
+     * lifecycle.
+     *
+     * @throws Throwable
+     *             In case any error occurs during the execution.
+     */
     public void run() throws Throwable {
         Optional<Interaction> interaction = findInteraction();
 
@@ -81,83 +109,223 @@ public class PactTestRunner {
 
     }
 
+    /**
+     * Gets a copy of the available {@link Pact}s.
+     *
+     * @return The availabe set of {@link Pact}s.
+     */
     public Set<Pact> getPacts() {
         return newHashSet(pacts);
     }
 
+    /**
+     * The consumer name to filter the {@link Pact}s.
+     *
+     * @return The configured consumer name for filtering purposes.
+     */
     public Optional<String> getConsumer() {
         return consumer;
     }
 
+    /**
+     * Sets the consumer name to filter the {@link Pact}s. Set to
+     * <code>null</code> to reset that filter and consider all {@link Pact}s.
+     *
+     * @param consumer
+     *            The name of the consumer to filter.
+     * @return Returns the current {@link PactTestRunner}.
+     */
     public PactTestRunner setConsumer(String consumer) {
         this.consumer = Optional.ofNullable(consumer);
         return this;
     }
 
+    /**
+     * The provider name to filter the {@link Pact}s.
+     *
+     * @return The configured provider name for filtering purposes.
+     */
     public Optional<String> getProvider() {
         return provider;
     }
 
+    /**
+     * Sets the provider name to filter the {@link Pact}s. Set to
+     * <code>null</code> to reset that filter and consider all {@link Pact}s.
+     *
+     * @param provider
+     *            The name of the provider to filter.
+     * @return Returns the current {@link PactTestRunner}.
+     */
     public PactTestRunner setProvider(String provider) {
         this.provider = Optional.ofNullable(provider);
         return this;
     }
 
+    /**
+     * The provider state that is used to retrieve the {@link Interaction} from
+     * the available {@link Pact}s.
+     *
+     * @return The configured provider state.
+     */
     public Optional<String> getProviderState() {
         return providerState;
     }
 
+    /**
+     * Sets the provider state to retrieve the {@link Interaction} from the
+     * available {@link Pact}s. Set to <code>null</code> to reset that value,
+     * but consider that a provider state is needed during execution to get the
+     * {@link Interaction}.
+     *
+     * @param providerState
+     *            The provider state to get the interaction for.
+     * @return Returns the current {@link PactTestRunner}.
+     */
     public PactTestRunner setProviderState(String providerState) {
         this.providerState = Optional.ofNullable(providerState);
         return this;
     }
 
+    /**
+     * The {@link MockMvc} that is used to execute the requests.
+     *
+     * @return The configured {@link MockMvc}.
+     */
     public Optional<MockMvc> getMockMvc() {
         return mockMvc;
     }
 
+    /**
+     * Sets the {@link MockMvc} to use for executing the request. This is
+     * required during execution.
+     *
+     * @param mockMvc
+     *            The {@link MockMvc} to use for executing the requests.
+     * @return Returns the current {@link PactTestRunner}.
+     */
     public PactTestRunner setMockMvc(MockMvc mockMvc) {
         this.mockMvc = Optional.ofNullable(mockMvc);
         return this;
     }
 
+    /**
+     * The context path of the requests.
+     *
+     * @return The configured context path.
+     */
     public Optional<String> getContextPath() {
         return contextPath;
     }
 
+    /**
+     * Sets the context path to use for executing the requests. Set to
+     * <code>null</code> to reset the context path.
+     *
+     * @param contextPath
+     *            The context path of the requests.
+     * @return Returns the current {@link PactTestRunner}.
+     */
     public PactTestRunner setContextPath(String contextPath) {
         this.contextPath = Optional.ofNullable(contextPath);
         return this;
     }
 
+    /**
+     * The currently configured request callback that is called during execution
+     * lifecycle to modify the request.
+     *
+     * @return The configured request callback.
+     */
     public Consumer<? super MockHttpServletRequestBuilder> getRequestCallback() {
         return requestCallback;
     }
 
-    public void setRequestCallback(Consumer<? super MockHttpServletRequestBuilder> requestCallback) {
+    /**
+     * Sets the request callback to call during execution lifecycle to modify
+     * the request before the request is sent.
+     *
+     * @param requestCallback
+     *            The callback to use.
+     * @return Returns the current {@link PactTestRunner}.
+     */
+    public PactTestRunner setRequestCallback(Consumer<? super MockHttpServletRequestBuilder> requestCallback) {
         this.requestCallback = requestCallback;
+        return this;
     }
 
+    /**
+     * The currently configured {@link ResultMatcher}s that are additionally
+     * used to verify the result.
+     *
+     * @return The configured additional {@link ResultMatcher}s.
+     */
     public Set<ResultMatcher> getResultMatchers() {
         return resultMatchers;
     }
 
-    public void addResultMatchers(Collection<ResultMatcher> resultMatchers) {
+    /**
+     * Adds additional {@link ResultMatchers} that will be used to verify the
+     * response. These {@link ResultMatcher}s are verified additional to the
+     * matchers generated from the pact response description.
+     *
+     * @param resultMatchers
+     *            The additional {@link ResultMatcher}s to use.
+     * @return Returns the current {@link PactTestRunner}.
+     */
+    public PactTestRunner addResultMatchers(Collection<ResultMatcher> resultMatchers) {
         this.resultMatchers.addAll(resultMatchers);
+        return this;
     }
 
-    public void addResultMatchers(ResultMatcher... resultMatchers) {
+    /**
+     * Adds additional {@link ResultMatchers} that will be used to verify the
+     * response. These {@link ResultMatcher}s are verified additional to the
+     * matchers generated from the pact response description.
+     *
+     * @param resultMatchers
+     *            The additional {@link ResultMatcher}s to use.
+     * @return Returns the current {@link PactTestRunner}.
+     */
+    public PactTestRunner addResultMatchers(ResultMatcher... resultMatchers) {
         this.resultMatchers.addAll(newHashSet(resultMatchers));
+        return this;
     }
 
+    /**
+     * The currently configured response callback that is called during
+     * execution lifecycle to access the response of the call.
+     *
+     * @return The configured response callback.
+     */
     public Consumer<? super ResultActions> getResponseCallback() {
         return responseCallback;
     }
 
-    public void setResponseCallback(Consumer<? super ResultActions> responseCallback) {
+    /**
+     * Sets the response callback to call during execution lifecycle to access
+     * the response of the call.
+     *
+     * @param responseCallback
+     *            The callback to use.
+     * @return Returns the current {@link PactTestRunner}.
+     */
+    public PactTestRunner setResponseCallback(Consumer<? super ResultActions> responseCallback) {
         this.responseCallback = responseCallback;
+        return this;
     }
 
+    /**
+     * Find the interaction from the available {@link Pact}s by first applying
+     * any configured consumer / provider filter and then determine the
+     * {@link Interaction} from the remaining {@link Pact}s by the provider
+     * state.
+     *
+     * @return The found {@link Interaction} or an empty {@link Optional} if no
+     *         interaction matches the provider state.
+     * @throws IllegalStateException
+     *             In case no provider state is configured.
+     */
     protected Optional<Interaction> findInteraction() {
         String state = providerState.orElseThrow(() -> new IllegalStateException(
                 "No provider state defined. Set one explicitly or use the ProviderState annotation"));
@@ -174,6 +342,13 @@ public class PactTestRunner {
         return ConversionUtils.getInteraction(pactStream, state);
     }
 
+    /**
+     * Determines the default {@link ResultMatcher}s from the Interaction.
+     *
+     * @param interaction
+     *            The interactions to get the matchers from.
+     * @return The default {@link ResultMatcher}s.
+     */
     protected Set<ResultMatcher> responseMatchers(Interaction interaction) {
         RequestResponseInteraction reqResInteraction;
         if (interaction instanceof RequestResponseInteraction) {
@@ -190,6 +365,13 @@ public class PactTestRunner {
         return responseMatchers(response);
     }
 
+    /**
+     * Determines the default {@link ResultMatcher}s from the {@link Response}.
+     *
+     * @param response
+     *            The {@link Response} to get the matchers from.
+     * @return The default {@link ResultMatcher}s.
+     */
     protected Set<ResultMatcher> responseMatchers(Response response) {
         Set<ResultMatcher> result = Sets.newHashSet();
 
